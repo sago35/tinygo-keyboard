b21901758e5bdc4f7c225c1b9180b70267a1a37c
sago35 <sago35@gmail.com>
2022-12-12 21:05:13 +0900

    Add mouse.go

---
+++ mouse.go
@@ -0,0 +1,15 @@
+package keyboard
+
+import (
+	"machine/usb/hid/mouse"
+)
+
+type Mouser interface {
+	Move(vx, vy int)
+	Click(btn mouse.Button)
+	Press(btn mouse.Button)
+	Release(btn mouse.Button)
+	Wheel(v int)
+	WheelDown()
+	WheelUp()
+}
