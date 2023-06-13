baf7959202ab0f9cbc4113f9c78c8e7339cf6569
sago35 <sago35@gmail.com>
2023-06-13 21:02:21 +0900

    Add display support for ./targets/sgkey

--- targets/sgkey/main.go
+++ targets/sgkey/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"image/color"
 	"log"
 	"machine"
 
 	keyboard "github.com/sago35/tinygo-keyboard"
 	"github.com/sago35/tinygo-keyboard/keycodes/jp"
+	"tinygo.org/x/drivers/ssd1306"
+	"tinygo.org/x/tinydraw"
 )
 
 func main() {
@@ -17,6 +21,18 @@ func main() {
 }
 
 func run() error {
+	machine.I2C0.Configure(machine.I2CConfig{
+		Frequency: machine.TWI_FREQ_400KHZ,
+	})
+
+	display := ssd1306.NewI2C(machine.I2C0)
+	display.Configure(ssd1306.Config{
+		Address: 0x3C,
+		Width:   128,
+		Height:  64,
+	})
+	display.ClearDisplay()
+
 	d := keyboard.New()
 
 	colPins := []machine.Pin{
@@ -30,11 +46,20 @@ func run() error {
 		machine.D2,
 	}
 
-	d.AddMatrixKeyboard(colPins, rowPins, [][][]keyboard.Keycode{
+	mk := d.AddMatrixKeyboard(colPins, rowPins, [][][]keyboard.Keycode{
 		{
 			{jp.KeyT, jp.KeyI, jp.KeyN},
 			{jp.KeyY, jp.KeyG, jp.KeyO},
 		},
+	})
+	mk.SetCallback(func(layer, row, col int, state keyboard.State) {
+		fmt.Printf("mk: %d %d %d %d\n", layer, row, col, state)
+		c := color.RGBA{255, 255, 255, 255}
+		if state == keyboard.PressToRelease {
+			c = color.RGBA{0, 0, 0, 255}
+		}
+		tinydraw.FilledRectangle(&display, 10+20*int16(col), 10+20*int16(row), 18, 18, c)
+		display.Display()
 	})
 
 	d.Debug = true
