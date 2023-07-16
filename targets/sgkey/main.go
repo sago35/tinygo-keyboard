2f35ca80134fe7b286a0a2d8f0b698b35c3ded86
sago35 <sago35@gmail.com>
2023-07-16 21:08:30 +0900

    WIP: targets/sgkey

--- targets/sgkey/main.go
+++ targets/sgkey/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"image/color"
 	"log"
 	"machine"
 
+	kbd "machine/usb/hid/keyboard"
+
 	keyboard "github.com/sago35/tinygo-keyboard"
 	"github.com/sago35/tinygo-keyboard/keycodes/jp"
-	"tinygo.org/x/drivers/ssd1306"
-	"tinygo.org/x/tinydraw"
 )
 
 func main() {
@@ -21,17 +20,10 @@ func main() {
 }
 
 func run() error {
-	machine.I2C0.Configure(machine.I2CConfig{
-		Frequency: machine.TWI_FREQ_400KHZ,
-	})
-
-	display := ssd1306.NewI2C(machine.I2C0)
-	display.Configure(ssd1306.Config{
-		Address: 0x3C,
-		Width:   128,
-		Height:  64,
-	})
-	display.ClearDisplay()
+	err := kbd.Load()
+	if err != nil {
+		return err
+	}
 
 	d := keyboard.New()
 
@@ -54,12 +46,6 @@ func run() error {
 	})
 	mk.SetCallback(func(layer, row, col int, state keyboard.State) {
 		fmt.Printf("mk: %d %d %d %d\n", layer, row, col, state)
-		c := color.RGBA{255, 255, 255, 255}
-		if state == keyboard.PressToRelease {
-			c = color.RGBA{0, 0, 0, 255}
-		}
-		tinydraw.FilledRectangle(&display, 10+20*int16(col), 10+20*int16(row), 18, 18, c)
-		display.Display()
 	})
 
 	d.Debug = true
