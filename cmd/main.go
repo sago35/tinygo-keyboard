aa37f5229fdd46989d924aad49cfa6a4b88480fb
akif999 <aki01nf28@gmail.com>
2023-08-19 10:45:31 +0900

    implement generating def.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,21 +3,65 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/itchio/lzma"
 )
 
 func main() {
-	data := []byte{0x01, 0x02, 0x03, 0x04}
-	fmt.Println(data)
+	if len(os.Args) != 2 {
+		log.Fatal("this program needs vial.json's path")
+	}
+	fmt.Println(os.Args[1])
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	r, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// compress
-	var b bytes.Buffer
-	w := lzma.NewWriter(&b)
-	_, err := w.Write(data)
+	var tBuf bytes.Buffer
+	w := lzma.NewWriter(&tBuf)
+	_, err = w.Write(r)
 	if err != nil {
 		fmt.Printf("lzma.Write() failed: %s", err.Error())
 	}
 	w.Close()
-	fmt.Println(b.Bytes())
+
+	var oBuf strings.Builder
+	oBuf.WriteString("package main\n")
+	oBuf.WriteString("\n")
+	oBuf.WriteString("import keyboard \"github.com/sago35/tinygo-keyboard\"\n")
+	oBuf.WriteString("\n")
+	oBuf.WriteString("func loadKeyboardDef() {\n")
+	oBuf.WriteString("\tkeyboard.KeyboardDef = []byte{\n")
+	oBuf.WriteString("\t\t")
+	for i, b := range tBuf.Bytes() {
+		if i == (len(tBuf.Bytes()) - 1) {
+			oBuf.WriteString(fmt.Sprintf("0x%02X,", b))
+		} else {
+			oBuf.WriteString(fmt.Sprintf("0x%02X, ", b))
+		}
+	}
+	oBuf.WriteString("\n")
+	oBuf.WriteString("\t}\n")
+	oBuf.WriteString("}\n")
+
+	// err = os.WriteFile(`../targets/sgkb/left-0.3.0/def.go`, []byte(oBuf.String()), 0666)
+	outPath := filepath.Join(path.Dir(os.Args[1]), `def.go`)
+	fmt.Println(outPath)
+	err = os.WriteFile(outPath, []byte(oBuf.String()), 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println(tBuf.Bytes())
 }
