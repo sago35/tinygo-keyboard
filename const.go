a7d3f463f31c37e7d6f72abfd32d0ce7b9907003
sago35 <sago35@gmail.com>
2023-08-23 18:31:38 +0900

    Update vial.json (rows / cols)

--- const.go
+++ const.go
@@ -5,5 +5,5 @@ const (
 
 	// This value must also be set in `matrix.cols` in vial.json.
 	// Keeping this value as a multiple of 14 is more efficient for vial communication.
-	MaxKeyCount = 100
+	MaxKeyCount = 252
 )
