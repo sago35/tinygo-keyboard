24e53429513e54a966e3fd74a7be97d0b87edf83
sago35 <sago35@gmail.com>
2023-09-01 18:56:04 +0900

    Remove unused const value

--- const.go
+++ const.go
@@ -2,8 +2,4 @@ package keyboard
 
 const (
 	LayerCount = 6
-
-	// This value must also be set in `matrix.cols` in vial.json.
-	// Keeping this value as a multiple of 14 is more efficient for vial communication.
-	MaxKeyCount = 252
 )
