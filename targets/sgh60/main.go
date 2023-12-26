732ad18710d6b26b442815365114770114312ca9
sago35 <sago35@gmail.com>
2023-12-26 10:11:57 +0900

    Update ./targets/sgh60/

--- targets/sgh60/main.go
+++ targets/sgh60/main.go
@@ -24,10 +24,8 @@ func main() {
 
 func run() error {
 	machine.InitADC()
-	ax := machine.ADC{Pin: machine.A2}
-	ax.Configure(machine.ADCConfig{})
-	ay := machine.ADC{Pin: machine.A3}
-	ay.Configure(machine.ADCConfig{})
+	ax := machine.A2
+	ay := machine.A3
 
 	m := mouse.Port()
 
@@ -51,21 +49,21 @@ func run() error {
 			jp.KeyTab, jp.KeyQ, jp.KeyW, jp.KeyE, jp.KeyR, jp.KeyT, jp.KeyY, jp.KeyU, jp.KeyI, jp.KeyO, jp.KeyP, jp.KeyAt, jp.KeyLeftBrace, jp.KeyEnter,
 			jp.KeyLeftCtrl, jp.KeyA, jp.KeyS, jp.KeyD, jp.KeyF, jp.KeyG, jp.KeyH, jp.KeyJ, jp.KeyK, jp.KeyL, jp.KeySemicolon, jp.KeyColon, jp.KeyRightBrace,
 			jp.KeyLeftShift, jp.KeyZ, jp.KeyX, jp.KeyC, jp.KeyV, jp.KeyB, jp.KeyN, jp.KeyM, jp.KeyComma, jp.KeyPeriod, jp.KeySlash, jp.KeyBackslash, jp.KeyUp, jp.KeyDelete,
-			jp.KeyMod1, jp.KeyLeftCtrl, jp.KeyWindows, jp.KeyLeftAlt, jp.KeyMod1, jp.KeySpace, jp.KeySpace, jp.KeyMod2, jp.KeyKana, jp.KeyLeftAlt, jp.KeyPrintscreen, jp.KeyLeft, jp.KeyDown, jp.KeyRight,
+			jp.KeyMod1, jp.KeyLeftCtrl, jp.KeyWindows, jp.KeyLeftAlt, jp.KeyMod1, jp.KeySpace, jp.KeySpace, jp.KeyMod2, jp.KeyHiragana, jp.KeyLeftAlt, jp.KeyPrintscreen, jp.KeyLeft, jp.KeyDown, jp.KeyRight,
 		},
 		{
 			jp.KeyEsc, jp.KeyF1, jp.KeyF2, jp.KeyF3, jp.KeyF4, jp.KeyF5, jp.KeyF6, jp.KeyF7, jp.KeyF8, jp.KeyF9, jp.KeyF10, jp.KeyF11, jp.KeyF12, jp.KeyBackslash2, jp.KeyBackspace,
 			jp.KeyTab, jp.KeyQ, jp.KeyF15, jp.KeyEnd, jp.KeyF17, jp.KeyF18, jp.KeyY, jp.KeyU, jp.KeyTab, jp.KeyO, jp.WheelUp, jp.KeyAt, jp.KeyLeftBrace, jp.KeyEnter,
 			jp.KeyLeftCtrl, jp.KeyHome, jp.KeyS, jp.MouseRight, jp.MouseLeft, jp.MouseBack, jp.KeyLeft, jp.KeyDown, jp.KeyUp, jp.KeyRight, jp.KeyEnter, jp.KeyEsc, jp.KeyRightBrace,
 			jp.KeyLeftShift, jp.KeyF13, jp.KeyF14, jp.MouseMiddle, jp.KeyF16, jp.MouseForward, jp.WheelDown, jp.KeyM, jp.KeyComma, jp.KeyPeriod, jp.KeySlash, jp.KeyBackslash, jp.KeyPageUp, jp.KeyDelete,
-			jp.KeyMod1, jp.KeyLeftCtrl, jp.KeyWindows, jp.KeyLeftAlt, jp.KeyMod1, jp.KeySpace, jp.KeySpace, jp.KeyMod2, jp.KeyKana, jp.KeyLeftAlt, jp.KeyPrintscreen, jp.KeyHome, jp.KeyPageDown, jp.KeyEnd,
+			jp.KeyMod1, jp.KeyLeftCtrl, jp.KeyWindows, jp.KeyLeftAlt, jp.KeyMod1, jp.KeySpace, jp.KeySpace, jp.KeyMod2, jp.KeyHiragana, jp.KeyLeftAlt, jp.KeyPrintscreen, jp.KeyHome, jp.KeyPageDown, jp.KeyEnd,
 		},
 		{
 			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 			jp.KeyTab, jp.Key1, jp.Key2, jp.Key3, jp.Key4, jp.Key5, jp.Key6, jp.Key7, jp.Key8, jp.Key9, jp.Key0, jp.KeyBackspace, 0, 0,
 			jp.KeyLeftCtrl, jp.KeyMinus, jp.KeyHat, jp.KeyBackslash2, jp.KeyLeftBrace, jp.KeyRightBrace, jp.KeyHome, jp.KeyPageDown, jp.KeyPageUp, jp.KeyEnd, jp.KeyEnter, jp.KeyEsc, 0,
 			jp.KeyLeftShift, jp.KeyF1, jp.KeyF2, jp.KeyF3, jp.KeyF4, jp.KeyF5, jp.KeyF6, jp.KeyF7, jp.KeyF8, jp.KeyF9, jp.KeyF10, jp.KeyF11, 0, 0,
-			jp.KeyMod1, jp.KeyLeftCtrl, jp.KeyWindows, jp.KeyLeftAlt, jp.KeyMod1, jp.KeySpace, jp.KeySpace, jp.KeyMod2, jp.KeyKana, jp.KeyLeftAlt, jp.KeyPrintscreen, jp.KeyF12, 0, 0,
+			jp.KeyMod1, jp.KeyLeftCtrl, jp.KeyWindows, jp.KeyLeftAlt, jp.KeyMod1, jp.KeySpace, jp.KeySpace, jp.KeyMod2, jp.KeyHiragana, jp.KeyLeftAlt, jp.KeyPrintscreen, jp.KeyF12, 0, 0,
 		},
 	})
 	sm.SetCallback(func(layer, index int, state keyboard.State) {
@@ -83,75 +81,22 @@ func run() error {
 	}
 
 	cont := true
+	x := NewADCDevice(ax, 0x3400, 0xD400, true)
+	y := NewADCDevice(ay, 0x3800, 0xE990, true)
 	for cont {
 		err := d.Tick()
 		if err != nil {
 			return err
 		}
 
-		x := ax.Get()
-		y := ay.Get()
+		xx := x.Get2()
+		yy := y.Get2()
 
-		xx := ad2xy(x, 0x7E00)
-		yy := ad2xy(y, 0x8600)
-		m.Move(-1*xx, -1*yy)
+		fmt.Printf("%04X %04X %4d %4d %4d %4d\n", x.RawValue, y.RawValue, xx, yy, x.Get(), y.Get())
+		m.Move(int(xx), int(yy))
 
 		time.Sleep(10 * time.Millisecond)
 	}
 
 	return nil
-}
-
-func ad2xy(x, initValue uint16) int {
-	xx := int(x) - int(initValue)
-
-	switch {
-	case abs(xx) < 0x1000:
-		return 0
-	case abs(xx) < 0x2000:
-		return 1 * sign(xx)
-	case abs(xx) < 0x3000:
-		return 2 * sign(xx)
-	case abs(xx) < 0x3500:
-		return 3 * sign(xx)
-	case abs(xx) < 0x3A00:
-		return 4 * sign(xx)
-	case abs(xx) < 0x4000:
-		return 5 * sign(xx)
-	case abs(xx) < 0x4500:
-		return 6 * sign(xx)
-	case abs(xx) < 0x4A00:
-		return 7 * sign(xx)
-	case abs(xx) < 0x5000:
-		return 8 * sign(xx)
-	case abs(xx) < 0x5500:
-		return 9 * sign(xx)
-	case abs(xx) < 0x5A00:
-		return 10 * sign(xx)
-	case abs(xx) < 0x6000:
-		return 11 * sign(xx)
-	case abs(xx) < 0x6400:
-		return 12 * sign(xx)
-	case abs(xx) < 0x6800:
-		return 13 * sign(xx)
-	case abs(xx) < 0x6C00:
-		return 14 * sign(xx)
-	case abs(xx) < 0x7000:
-		return 15 * sign(xx)
-	}
-	return 20 * sign(xx)
-}
-
-func sign(x int) int {
-	if x < 0 {
-		return -1
-	}
-	return 1
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -1 * x
-	}
-	return x
 }
