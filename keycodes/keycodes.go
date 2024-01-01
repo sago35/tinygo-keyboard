bfe94b187b17c52a81bb7e44afbc1e46d07332ae
sago35 <sago35@gmail.com>
2024-01-01 17:42:26 +0900

    Refactor keycodes

--- keycodes/keycodes.go
+++ keycodes/keycodes.go
@@ -6,31 +6,39 @@ const (
 )
 
 const (
-	KeyLeftCtrl   = 0xF000 | 0xE0
-	KeyLeftShift  = 0xF000 | 0xE1
-	KeyLeftAlt    = 0xF000 | 0xE2
-	KeyWindows    = 0xF000 | 0xE3
-	KeyRightCtrl  = 0xF000 | 0xE4
-	KeyRightShift = 0xF000 | 0xE5
-
-	KeyH         = 0xF000 | 0x0B
-	KeyBackspace = 0xF000 | 0x2A
+	TypeMediaKey = 0xE400
+	TypeNormal   = 0xF000
+	TypeMouse    = 0xD000
+	TypeModKey   = 0xFF00
+	TypeToKey    = 0xFF10
 )
 
 const (
-	KeyMod0 = ModKeyMask | 0x00
-	KeyMod1 = ModKeyMask | 0x01
-	KeyMod2 = ModKeyMask | 0x02
-	KeyMod3 = ModKeyMask | 0x03
-	KeyMod4 = ModKeyMask | 0x04
-	KeyMod5 = ModKeyMask | 0x05
+	KeyLeftCtrl   = TypeNormal | 0xE0
+	KeyLeftShift  = TypeNormal | 0xE1
+	KeyLeftAlt    = TypeNormal | 0xE2
+	KeyWindows    = TypeNormal | 0xE3
+	KeyRightCtrl  = TypeNormal | 0xE4
+	KeyRightShift = TypeNormal | 0xE5
 
-	KeyTo0 = ModKeyMask | ToKeyMask | 0x00
-	KeyTo1 = ModKeyMask | ToKeyMask | 0x01
-	KeyTo2 = ModKeyMask | ToKeyMask | 0x02
-	KeyTo3 = ModKeyMask | ToKeyMask | 0x03
-	KeyTo4 = ModKeyMask | ToKeyMask | 0x04
-	KeyTo5 = ModKeyMask | ToKeyMask | 0x05
+	KeyH         = TypeNormal | 0x0B
+	KeyBackspace = TypeNormal | 0x2A
+)
+
+const (
+	KeyMod0 = TypeModKey | 0x00
+	KeyMod1 = TypeModKey | 0x01
+	KeyMod2 = TypeModKey | 0x02
+	KeyMod3 = TypeModKey | 0x03
+	KeyMod4 = TypeModKey | 0x04
+	KeyMod5 = TypeModKey | 0x05
+
+	KeyTo0 = TypeToKey | 0x00
+	KeyTo1 = TypeToKey | 0x01
+	KeyTo2 = TypeToKey | 0x02
+	KeyTo3 = TypeToKey | 0x03
+	KeyTo4 = TypeToKey | 0x04
+	KeyTo5 = TypeToKey | 0x05
 )
 
 const (
