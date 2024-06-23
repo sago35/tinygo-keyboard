7936b0566c2aecefcc04b6c15dd639710a1490a9
sago35 <sago35@gmail.com>
2024-06-23 14:09:35 +0900

    WIP

--- targets/xiao-kb01/main.go
+++ targets/xiao-kb01/main.go
@@ -8,12 +8,14 @@ import (
 	"machine"
 	"machine/usb"
 	"math/rand"
+	"runtime/interrupt"
 	"runtime/volatile"
 	"time"
 
 	keyboard "github.com/sago35/tinygo-keyboard"
 	"github.com/sago35/tinygo-keyboard/keycodes/jp"
-	"tinygo.org/x/drivers/ws2812"
+	pio "github.com/tinygo-org/pio/rp2-pio"
+	"github.com/tinygo-org/pio/rp2-pio/piolib"
 )
 
 func main() {
@@ -27,6 +29,7 @@ func main() {
 
 var (
 	white = color.RGBA{0x3F, 0x3F, 0x3F, 0xFF}
+	red   = color.RGBA{0xFF, 0x00, 0x00, 0xFF}
 	black = color.RGBA{0x00, 0x00, 0x00, 0xFF}
 )
 
@@ -35,12 +38,13 @@ func run() error {
 	changed.Set(0)
 
 	neo := machine.D4
-	neo.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	ws := ws2812.New(neo)
+	s, _ := pio.PIO0.ClaimStateMachine()
+	ws, _ := piolib.NewWS2812(s, neo)
 	wsLeds := [12]color.RGBA{}
 	for i := range wsLeds {
 		wsLeds[i] = black
 	}
+	writeColors(s, ws, wsLeds[:])
 
 	d := keyboard.New()
 
@@ -67,6 +71,7 @@ func run() error {
 			rowx = 3 - row - 1
 		}
 		c := rand.Int()
+		mask := interrupt.Disable()
 		wsLeds[rowx+3*col] = color.RGBA{
 			byte(c>>16) & 0x3F,
 			byte(c>>8) & 0x3F,
@@ -74,8 +79,9 @@ func run() error {
 			0xFF,
 		}
 		if state == keyboard.PressToRelease {
-			wsLeds[rowx+3*col] = black
+			//wsLeds[rowx+3*col] = black
 		}
+		interrupt.Restore(mask)
 		fmt.Printf("%#v\n", wsLeds)
 		changed.Set(1)
 	})
@@ -86,10 +92,48 @@ func run() error {
 		},
 	})
 
-	d.AddRotaryKeyboard(machine.D9, machine.D8, [][]keyboard.Keycode{
+	r2 := d.AddRotaryKeyboard(machine.D9, machine.D8, [][]keyboard.Keycode{
 		{
 			jp.WheelDown, jp.WheelUp,
 		},
+	})
+	r2idx := 0
+	r2.SetCallback(func(layer, index int, state keyboard.State) {
+		if state == 2 {
+			if index == 1 {
+				r2idx = (r2idx + 1) % 10
+			} else {
+				r2idx = (r2idx - 1 + 10) % 10
+			}
+			redidx := r2idx
+			switch r2idx {
+			case 0:
+				redidx = 0
+			case 1:
+				redidx = 1
+			case 2:
+				redidx = 2
+			case 3:
+				redidx = 3
+			case 4:
+				redidx = 8
+			case 5:
+				redidx = 9
+			case 6:
+				redidx = 10
+			case 7:
+				redidx = 11
+			case 8:
+				redidx = 6
+			case 9:
+				redidx = 5
+			}
+			fmt.Printf("r2: %d %d %d %d\n", layer, index, r2idx, state)
+			mask := interrupt.Disable()
+			wsLeds[redidx] = red
+			interrupt.Restore(mask)
+			changed.Set(1)
+		}
 	})
 
 	gpioPins := []machine.Pin{machine.D7, machine.D6}
@@ -121,12 +165,31 @@ func run() error {
 		}
 		if cnt%4 == 0 {
 			if changed.Get() != 0 {
-				ws.WriteColors(wsLeds[:])
+				writeColors(s, ws, wsLeds[:])
 				changed.Set(0)
 			}
+		}
+		if cnt%32 == 0 {
+			mask := interrupt.Disable()
+			for i, c := range wsLeds {
+				c.B >>= 1
+				c.R >>= 1
+				c.G >>= 1
+				wsLeds[i] = c
+			}
+			writeColors(s, ws, wsLeds[:])
+			interrupt.Restore(mask)
 		}
 		cnt++
 	}
 
 	return nil
+}
+
+func writeColors(s pio.StateMachine, ws *piolib.WS2812, colors []color.RGBA) {
+	for _, c := range colors {
+		for s.IsTxFIFOFull() {
+		}
+		ws.SetColor(c)
+	}
 }
